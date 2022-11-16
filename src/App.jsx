b8b534e9bdfc05f57cd1df85cb22d2df0fc4ade2
VictorIbarraOlivares <victor.ibarra.olivares@gmail.com>
2022-11-15 22:35:55 -0300

    primer deploy

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>Inicio proyecto WEB CIMOSALUD</h2>
+      <h2>Inicio proyecto WEB CIMOSALUD !</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
