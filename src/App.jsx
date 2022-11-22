95859bad40f825fb7e8d3ffe285ef2a50f477716
VictorIbarraOlivares <victor.ibarra.olivares@gmail.com>
2022-11-21 23:59:13 -0300

    Se crea navegador

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,26 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h2>Inicio proyecto WEB CIMOSALUD !</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav class="nav">
+        <h1 class="nav__logo">CIMO</h1>
+        <ul class="nav__list">
+          <li class="nav__item">
+            <a href="" class="nav__link">Inicio</a>
+          </li>
+          <li class="nav__item">
+            <a href="" class="nav__link">Nosotros</a>
+          </li>
+          <li class="nav__item">
+            <a href="" class="nav__link">Servicios & Precios</a>
+          </li>
+          <li class="nav__item">
+            <a href="" class="nav__link">Contacto</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   )
 }
