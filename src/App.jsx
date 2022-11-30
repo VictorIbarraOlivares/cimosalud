bd46ec737588c7d1fb2c6ef191db485fed13e76e
VictorIbarraOlivares <victor.ibarra.olivares@gmail.com>
2022-11-30 00:07:20 -0300

    ADD cambios en en navegador y se agrega imagen de fondo

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,22 @@ import './App.css'
 
 function App() {
 
+  const [openHamburguer, setOpenHamburguer] = useState(false);
+
   return (
     <div className="App">
-      <nav className="nav">
+      <nav
+        className={`nav ${openHamburguer && 'open'}`}>
         <div className='brand__title'>CIMO</div>
-        <a href="#" className='toggle-button'>
+        <div 
+          className={`toggle-button ${openHamburguer && 'open'}`} 
+          onClick={() => setOpenHamburguer(!openHamburguer)}>
           <span className='bar'></span>
           <span className='bar'></span>
           <span className='bar'></span>
-        </a>
-        <div className='nav__navigation'>
+        </div>
+        <div 
+          className={`nav__navigation ${openHamburguer && 'open'}`} >
           <ul className="nav__list">
             <li className="nav__item">
               <a href="" className="nav__link">Inicio</a>
@@ -30,49 +36,8 @@ function App() {
           </ul>
         </div>
       </nav>
-      {/* <div className='header'>
-        <div className='header__container'>
-          <div className='header__elements'>
-            <div className='header__logo'>
-              <h1 className="">CIMO</h1>
-              <span className='span__logo'>logo</span>
-
-            </div>
-
-            <div className='header__navigation'>
-              <nav className="nav">
-                <div className='nav__navigation'>
-                  <ul className="nav__list">
-                    <li className="nav__item">
-                      <a href="" className="nav__link">Inicio</a>
-                    </li>
-                    <li className="nav__item">
-                      <a href="" className="nav__link">Nosotros</a>
-                    </li>
-                    <li className="nav__item">
-                      <a href="" className="nav__link">Servicios & Precios</a>
-                    </li>
-                    <li className="nav__item">
-                      <a href="" className="nav__link">Contacto</a>
-                    </li>
-                  </ul>
-                </div>
-              </nav>
-
-
-              <div className='header__tool'>
-                <div className='header__tool__phone'>
-                  <a href='tel:+56964338091'>+56 9 6433 8091</a>
-                </div>
-
-                <div className='header__tool_button'>
-                  <a href="https://api.whatsapp.com/send?phone=56964338091&text=" target="_blank">Reserva una cita</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
+      <div className='background__image'>
+      </div>
     </div>
   )
 }
